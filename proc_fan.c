78de48af603a777227c5ca492b2544eec35e71a1
mbeckering <mikebeckering@gmail.com>
2018-02-06 13:24:57 -0600

    Update proc_fan.c

--- proc_fan.c
+++ proc_fan.c
@@ -8,10 +8,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
+//simple function to print proc_fan usage
 void print_usage() {
     printf("Usage: proc_fan -n <# of processes>\n");
 }
@@ -24,7 +23,7 @@ int main(int argc, char *argv[]) {
     pid_t childpid =0;
     int i, n;
     
-    //getopt loop to parse command line args
+    //getopt loop to parse command line options
     while ((option = getopt(argc, argv, "hn")) != -1) {
         switch(option) {
             case 'h':
@@ -50,6 +49,7 @@ int main(int argc, char *argv[]) {
         else if ( (nflag == 1) && (hflag == 0) ) {
             if ( argv[2] != NULL && ( n = atoi(argv[2]) != 0) ) { //...as long as a nonzero int was entered
             printf("calling testsim with arg %d\n", n);
+            //ENTER TESTSIM CALL HERE********************
             return 0;
             }
             //exit with error if there was no -n arg
@@ -63,19 +63,8 @@ int main(int argc, char *argv[]) {
             print_usage();
             return 0;
         }
-    
-        
-        /* move to testsim?
-        n = atoi(argv[1]);
-
-        for (i=1; i<n; i++) {
-            if ( (childpid = fork()) <= 0 )
-                break;
-        }
-    
-        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-                i, (long)getpid(), (long)getppid, (long)childpid);
-        */
 
     return 0;
 }
+
+

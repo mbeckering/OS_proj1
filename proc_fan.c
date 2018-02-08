4050bc21de9ba7d5fe6a0344cb57c1e5a68d6d84
mbeckering <mikebeckering@gmail.com>
2018-02-08 13:18:52 -0600

    Update proc_fan.c

--- proc_fan.c
+++ proc_fan.c
@@ -4,6 +4,7 @@
  * CS-4760-E01-SP18
  * Project 1
  * Created on January 31, 2018, 9:15 AM
+ *Last edited on February 8, 2018, 11:53 AM
  */
 
 #include <stdio.h>
@@ -51,45 +52,54 @@ int main(int argc, char *argv[]) {
         
         //if only -n is selected, proceed with -n arg...
         else if ( (nflag == 1) && (hflag == 0) ) {
-            if ( argv[2] != NULL && ( (pr_limit = atoi(argv[2])) != 0) ) { //...as long as a nonzero int was entered
+            //...IF a positive int was entered...
+            if ( argv[2] != NULL && ( (pr_limit = atoi(argv[2])) > 0) ) {
             
-            char command[20];
-            char data[20];
+            char command[20]; //string for calling testsim
+            char data[20]; //string to store data from testing.data file
             
             //open file stream
             FILE *fp;
             fp = fopen("testing.data", "r");
             
+            //exit if data file is not found
+            if (fp == NULL) {
+                perror("Error opening testing.data: ");
+                return 0;
+            }
+            
             //***MAIN LOOP***
             while (fgets(data, 20, fp) != NULL) {
+                //wait for one child to finish if process limit is reached
                 if (pr_count == pr_limit) {
                     wait(NULL);
                     pr_count--;
                 }
+                //create string for call to testsim
                 strcpy(command, "./");
                 strcat(command, data);
+                //fork here
                 if ( (childpid = fork()) <=0 ){
-                    system(command);//run testsim
-                    return 0;
+                    system(command);//this is the child calling testsim
+                    return 0;//child exits when testsim completes
                 }
-                pr_count++;
+                pr_count++; //parent continues and increments current process count
             }
 
-            fclose(fp);
-            printf("PROC_FAN: Exited main loop, waiting for all children to complete\n");
-            while ( (wpid = wait(&status)) > 0);
-            
+            fclose(fp); //close file stream
+            while ( (wpid = wait(&status)) > 0); //wait for all children to finish
             printf("PROC_FAN: All children terminated.\n");
             return 0;
             }
-            //exit with error if there was no -n arg
+            
+            //exit with error if there was no argument after -n
             else { 
-                printf("%s: Error: -n requires an integer argument\n", argv[0]);
+                printf("%s: Error: -n requires a positive integer argument\n", argv[0]);
                 printf("Usage: %s -n <# of processes>\n", argv[0]);
             }
         }
         
-        //Otherwise, exit with error message 
+        //Exit with error message if user didn't select either -h or -n options
         else {
             printf("%s: Error: invalid option selection\n", argv[0]);
             printf("Usage: %s -n <# of processes>\n", argv[0]);

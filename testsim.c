5190728bfe8fd7fdfd62e8083291957a6563e0b2
mbeckering <mikebeckering@gmail.com>
2018-02-08 11:36:33 -0600

    Update testsim.c
    workinf form, just need to clean & comment

--- testsim.c
+++ testsim.c
@@ -14,11 +14,16 @@ int main(int argc, char *argv[]) {
     int sleep_time = atoi(argv[1]);
     int repeat_factor = atoi(argv[2]);
     int i = 0;
-    pid_t mypid = 0;
+    pid_t mypid = getpid();
     
-    printf("RUNNING TESTSIM\n");
-    printf("sleep_time = %d\n", sleep_time);
-    printf("repeat_factor = %d\n", repeat_factor);
+    for (i=1; i<=repeat_factor; i++) {
+        if (i==1)
+        {printf("TESTSIM: pid %ld CREATED! Sleeping %d second/(s/) %d/%d times...\n", mypid, sleep_time, i, repeat_factor);}
+        else
+        {printf("TESTSIM: pid %ld sleeping %d seconds %d/%d times...\n", mypid, sleep_time, i, repeat_factor);}
+        sleep(sleep_time);
+    }
     
+    printf("TESTSIM: pid %ld terminating.\n", mypid);
     return 0;
 }
